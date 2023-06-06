41f0d4a2aac3df86e8f55e86eee67bd1122a9f79
Sicheng Liu <sichengliu@microsoft.com>
2023-06-06 15:59:10 +0800

    bootstrap of StickyNotes

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { MD3LightTheme as DefaultTheme, PaperProvider } from 'react-native-paper';
+import App from './components/app';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'tomato',
+    secondary: 'yellow',
   },
-});
+};
+
+export default function Main() {
+  return (
+    <PaperProvider  theme={theme}>
+      <App />
+    </PaperProvider>
+  );
+};
