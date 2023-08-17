5d597f49c7a69779d469c4e20eed8d69d34d0905
mt4c <mt4c@qq.com>
2023-08-17 16:52:15 +0800

    use masonry layout (#7)
    Co-authored-by: Chengyu Lv <chengyulv@microsoft.com>

--- components/profile.tsx
+++ components/profile.tsx
@@ -1,22 +1,39 @@
-import React, { useState, useRef, useCallback, useEffect } from 'react';
-import { Avatar, Surface, Card, PaperProvider, Button, Text, Divider, List, ActivityIndicator, MD2Colors } from 'react-native-paper';
-import { FontAwesome } from 'react-native-vector-icons';
-import { Note } from '../types/note';
-import { v4 as uuidv4 } from 'uuid';
-import { ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
-import { ConnectedProps, connect } from 'react-redux';
-import { AppDispatch, RootState } from '../redux/store';
-import { setIosEphemeralSession } from '../redux/authSlice';
-import { initAsync, loginAsync, logoutAsync, acquireTokenAsync } from '../redux/actions';
-import { MSALWebviewParams } from 'react-native-msal';
-import { NotesListStatus } from '../redux/actionType';
+import React, { useState, useRef, useCallback, useEffect } from "react";
+import {
+  Avatar,
+  Surface,
+  Card,
+  PaperProvider,
+  Button,
+  Text,
+  Divider,
+  List,
+  ActivityIndicator,
+  MD2Colors,
+} from "react-native-paper";
+import { FontAwesome } from "react-native-vector-icons";
+import { Note } from "../types/note";
+import { v4 as uuidv4 } from "uuid";
+import { ScrollView, StyleSheet, TouchableOpacity } from "react-native";
+import { ConnectedProps, connect } from "react-redux";
+import { AppDispatch, RootState } from "../redux/store";
+import { setIosEphemeralSession } from "../redux/authSlice";
+import {
+  initAsync,
+  loginAsync,
+  logoutAsync,
+  acquireTokenAsync,
+  GetAvatarAsync,
+} from "../redux/actions";
+import { MSALWebviewParams } from "react-native-msal";
+import { NotesListStatus } from "../redux/actionType";
 
 const mapState = (state: RootState) => ({
   AuthResult: state.authReducer.AuthResult,
   webviewParam: state.authReducer.webviewParameters,
   iosEphemeralSession: state.authReducer.iosEphemeralSession,
   status: state.authReducer.status,
-})
+});
 
 const mapDispatch = (dispatch: AppDispatch) => {
   return {
@@ -24,15 +41,18 @@ const mapDispatch = (dispatch: AppDispatch) => {
     initAsync: () => dispatch(initAsync()),
     loginAsync: (param: MSALWebviewParams) => dispatch(loginAsync(param)),
     logoutAsync: () => dispatch(logoutAsync()),
-    acquireTokenAsync: (forceRefresh: boolean) => dispatch(acquireTokenAsync(forceRefresh)),
-    setIosEphemeralSession: (val: boolean) => dispatch(setIosEphemeralSession(val))
-  }
-}
+    acquireTokenAsync: (forceRefresh: boolean) =>
+      dispatch(acquireTokenAsync(forceRefresh)),
+    setIosEphemeralSession: (val: boolean) =>
+      dispatch(setIosEphemeralSession(val)),
+    getAvatarAsync: () => dispatch(GetAvatarAsync()),
+  };
+};
 
 // init redux property
-const connector = connect(mapState, mapDispatch)
+const connector = connect(mapState, mapDispatch);
 
-type PropsFromRedux = ConnectedProps<typeof connector>
+type PropsFromRedux = ConnectedProps<typeof connector>;
 
 interface ProfileProps extends PropsFromRedux {
   navigation: any;
@@ -40,11 +60,18 @@ interface ProfileProps extends PropsFromRedux {
 }
 
 const ProfilePage = (props: ProfileProps) => {
+  const [avatar, setAvatar] = React.useState("");
 
   React.useEffect(() => {
     if (!props.AuthResult && props.status === NotesListStatus.idle) {
-      props.navigation.navigate('Login');
+      props.navigation.navigate("Login");
     }
+
+    props.getAvatarAsync().then((avatarUrl) => {
+      if (avatarUrl.payload != null) {
+        setAvatar(avatarUrl.payload as string);
+      }
+    });
   }, [props.AuthResult]);
 
   if (props.status === NotesListStatus.loading) {
@@ -52,62 +79,79 @@ const ProfilePage = (props: ProfileProps) => {
       <Surface style={styles.surface} elevation={4}>
         <ActivityIndicator animating={true} color={MD2Colors.red800} />
       </Surface>
-    )
-  }
-  
-    // TODO: butify this page
-    return (
-      <Surface style={styles.surface}>
-        <ScrollView style={styles.scrollView}>
-          <List.Section>
-            <List.Item title="Acquire Token" 
-              description="Refresh token for your Microsoft account" 
-              left={props => <List.Icon {...props} icon="account-clock" />}
-              onPress={() => props.acquireTokenAsync(true)}
-            />
-            <List.Item title="Sign Out" 
-              description="Sign out of current Microsoft account" 
-              left={props => <List.Icon {...props} icon="account-cancel" />}
-              onPress={props.logoutAsync}
-            />
-          </List.Section>
-          <Divider />
-          <Text>{JSON.stringify(props.AuthResult, null, 2)}</Text>
-        </ScrollView>
-      </Surface>
     );
-  };
+  }
 
-  const styles = StyleSheet.create({
-    surface: {
-      flex: 1,
-      marginTop: '0%',
-      marginBottom: '0%',
-      paddingBottom: '0%',
-      height: "100%",
-      width: "100%",
-      maxWidth: "100%",
-      maxHeight: "100%",
-      alignSelf: 'center',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    button: {
-      backgroundColor: 'aliceblue',
-      borderWidth: 1,
-      margin: '0.5%',
-      padding: 8,
-      width: '49%',
-      alignItems: 'center',
-    },
-    scrollView: {
-      padding: 1,
-      width: "100%",
-      height: "100%",
-      maxHeight: "100%",
-      maxWidth: "100%",
-      flex: 1,
-    },
-  });
-  
-  export default connector(ProfilePage);
+  // TODO: butify this page
+  return (
+    <Surface style={styles.surface}>
+      <ScrollView style={styles.scrollView}>
+        <List.Section>
+          {props.AuthResult ? (
+            <List.Item
+              title={(props.AuthResult.account.claims as any)?.name}
+              description={props.AuthResult.account.username}
+              left={(props) =>
+                avatar ? (
+                  <List.Icon style={props.style} icon={{ uri: avatar }} />
+                ) : (
+                  <List.Icon {...props} icon="account" />
+                )
+              }
+            />
+          ) : (
+            <></>
+          )}
+          <List.Item
+            title="Acquire Token"
+            description="Refresh token for your Microsoft account"
+            left={(props) => <List.Icon {...props} icon="account-clock" />}
+            onPress={() => props.acquireTokenAsync(true)}
+          />
+          <List.Item
+            title="Sign Out"
+            description="Sign out of current Microsoft account"
+            left={(props) => <List.Icon {...props} icon="account-cancel" />}
+            onPress={props.logoutAsync}
+          />
+        </List.Section>
+        <Divider />
+        <Text>{JSON.stringify(props.AuthResult, null, 2)}</Text>
+      </ScrollView>
+    </Surface>
+  );
+};
+
+const styles = StyleSheet.create({
+  surface: {
+    flex: 1,
+    marginTop: "0%",
+    marginBottom: "0%",
+    paddingBottom: "0%",
+    height: "100%",
+    width: "100%",
+    maxWidth: "100%",
+    maxHeight: "100%",
+    alignSelf: "center",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    backgroundColor: "aliceblue",
+    borderWidth: 1,
+    margin: "0.5%",
+    padding: 8,
+    width: "49%",
+    alignItems: "center",
+  },
+  scrollView: {
+    padding: 1,
+    width: "100%",
+    height: "100%",
+    maxHeight: "100%",
+    maxWidth: "100%",
+    flex: 1,
+  },
+});
+
+export default connector(ProfilePage);
